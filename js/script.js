221a4cc101ebd9b428e815d1221a80615f7b858c
SaraBarrett <sara.c.mon@gmail.com>
2025-05-14 10:25:53 +0100

    data types

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-alert('Olá Turma de Multimédia');
-alert('Superámos o primeiro dia de JS');
+//alert("Olá Turma de Multimédia");
+//alert('Superámos o primeiro dia de JS');
 
+//TIPOS DE DADOS EM JS
+//string
+//alert('Curso de Multimédia');
+//número
+//alert(122);
 
+//variáveis
+let courseName = "Técnico Multimédia";
 
-
-
-
+alert('o nome do curso é: ' +courseName);
+alert('as notas do '+courseName+' são');
+alert(courseName);
+alert(courseName);
