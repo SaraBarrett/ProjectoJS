6dd235594670e98b2d452e562ba703177ac83d60
SaraBarrett <sara.c.mon@gmail.com>
2025-05-14 12:58:07 +0100

    string metods

--- js/exs.js
+++ js/exs.js
@@ -1,5 +1,11 @@
 //pp 16
-let myLuckyNumber = 16;
-let myName = "Sara";
+// let myLuckyNumber = 16;
+// let myName = "Sara";
 
-alert("o meu nome é "+myName+" e o meu número é o "+myLuckyNumber);
+// alert("o meu nome é "+myName+" e o meu número é o "+myLuckyNumber);
+
+//pp 27
+const MESSAGE = " TASTE THE RAINBOW";
+alert(MESSAGE)
+let whisper = MESSAGE.trim().toLowerCase();
+alert(whisper)
--- js/script.js
+++ js/script.js
@@ -29,5 +29,16 @@ let mySurname = 'Monteiro';
 let myFullName= myFName + ' '+mySurname;
 
 //alert("o tamanho da variável é "+mySurname.length)
-alert(myFName.trim().toLowerCase());
+//alert(myFName.trim().toLowerCase());
 
+//métodos com argumentos
+console.log(myFName)
+console.log(myFullName)
+
+let ourCourse = 'Técnico de Multimédia';
+//ve a posição de i
+console.log("a letra i aparece na posição "+ourCourse.indexOf('i'));
+//corta Multimedia
+console.log("o curso é "+ourCourse.slice(11))
+//subistui Multimedia por OCOmercio
+console.log("o novo curso é "+ourCourse.replace("Multimédia", "Comércio"))
