6ecc42268ad71ee3ca52f5aa805137273d268d04
SaraBarrett <sara.c.mon@gmail.com>
2025-05-19 08:33:55 +0100

    ifs

--- js/exs.js
+++ js/exs.js
@@ -5,7 +5,34 @@
 // alert("o meu nome é "+myName+" e o meu número é o "+myLuckyNumber);
 
 //pp 27
-const MESSAGE = " TASTE THE RAINBOW";
-alert(MESSAGE)
-let whisper = MESSAGE.trim().toLowerCase();
-alert(whisper)
+// const MESSAGE = " TASTE THE RAINBOW";
+// alert(MESSAGE)
+// let whisper = MESSAGE.trim().toLowerCase();
+// alert(whisper)
+
+//pp29
+// const WORD = "skateboard";
+// let facialHair = WORD.slice(5).replace("o", "e");
+// console.log(facialHair);
+
+// let firstName = prompt("qual é o seu primeiro nome?");
+// let surname = prompt("qual é o seu primeiro Apelido?");
+// alert("olá "+firstName+" "+surname)
+
+//pp 39
+let dayOfWeek = prompt("que dia da semana é?").toLowerCase();
+
+if (dayOfWeek.slice(0, 5) == "sexta") {
+  alert("yay é sexta");
+}else if(dayOfWeek == 'sabado' || dayOfWeek == 'domingo' ||dayOfWeek == 'sábado'){
+    alert("fim de semana");
+}else {
+  alert("força nisso");
+}
+
+
+// let password= prompt("digite uma pass com pelo menos 6caracteres");
+
+// if(password.length<6){
+//     alert('pass inválida. tem que ter pelo menos 6caracteres')
+// }
--- js/script.js
+++ js/script.js
@@ -40,5 +40,33 @@ let ourCourse = 'Técnico de Multimédia';
 console.log("a letra i aparece na posição "+ourCourse.indexOf('i'));
 //corta Multimedia
 console.log("o curso é "+ourCourse.slice(11))
+
+
 //subistui Multimedia por OCOmercio
-console.log("o novo curso é "+ourCourse.replace("Multimédia", "Comércio"))
+console.log("o novo curso é "+ourCourse.replace("Multimédia", "Comércio") + "!");
+
+//sintaxe alternativa
+console.log(`o novo curso é ${ourCourse.replace("Multimédia", "Comércio")}!`);
+
+//tipo de dados null
+let myGrade = null;
+console.log("o null é " +myGrade)
+
+//tipo de dados undefinied
+// let myWeekend = sexta;
+// console.log(myWeekend)
+
+// let answer = prompt("querem ir para a praia?");
+// console.log(answer)
+
+const VOTE_AGE = 18;
+let userAge = prompt("qual é a sua idade?");
+
+if(userAge<"VOTE_AGE"){
+    alert("ainda não tens idade para votar");
+}else if(userAge == VOTE_AGE){
+    alert("vais votar pela primeira vez, parabéns")
+}else{
+    alert("vota em consciência no domingo!");
+}
+
