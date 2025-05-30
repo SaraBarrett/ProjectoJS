e3040350f2cc53dbcd4320c071032c1ffbd8a0a1
SaraBarrett <sara.c.mon@gmail.com>
2025-05-30 12:27:35 +0100

    fucntions

---
+++ js/functions.js
@@ -0,0 +1,38 @@
+
+
+function helloFunctions(){
+    alert('olá, aqui vamos às funções');
+}
+
+function letsGoShopping(){
+    let shoppingList=[];
+
+    let product = prompt("Qual o produto que quer adicionar à lista?");
+    let code = "fim"
+
+    while (product != code){
+        shoppingList.push(product);
+        product = prompt("Qual o produto que quer adicionar à lista?");
+    } 
+
+    for(let element of shoppingList){
+            console.log('o item é '+element)
+    }
+}
+
+function printHeart(){
+    alert('❤️');
+}
+
+function printHello(){
+    alert('Hello');
+}
+
+function printHelloTurma(){
+    alert('Turma Multimédia');
+}
+
+
+function printAlert(message){
+    alert(message);
+}
